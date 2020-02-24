7b0424b0fc8aa1a847fcb2b73a5d094448016e6c
SunBangchao <>
2020-02-24 13:57:33 +0800

    init backend code

---
+++ Back-end/spiderutil/src/main/java/com/colon/spiderutil/SpiderutilApplication.java
@@ -0,0 +1,13 @@
+package com.colon.spiderutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderutilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiderutilApplication.class, args);
+    }
+
+}
---
+++ Back-end/spiderutil/src/test/java/com/colon/spiderutil/SpiderutilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colon.spiderutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiderutilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
