877c72e4ce7685ad1f7331f9d0e02498785c5ffd
sun45400 <454066233@qq.com>
2020-02-24 14:37:19 +0800

    init mysql and mybatis config

--- Back-end/spiderutil/src/main/java/com/colon/spiderutil/SpiderutilApplication.java
+++ Back-end/spiderutil/src/main/java/com/colon/spiderutil/SpiderutilApplication.java
@@ -1,9 +1,11 @@
 package com.colon.spiderutil;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({"com.colon.spiderutil.mapper"})
 public class SpiderutilApplication {
 
     public static void main(String[] args) {
